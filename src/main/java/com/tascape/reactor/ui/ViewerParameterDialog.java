42979eb717ef55f17b4ae41c9f0d0f80e2788eca
Linsong Wang <tascape@gmail.com>
2016-07-19 23:18:01 -0700

    update group and artifact ids

--- src/main/java/com/tascape/reactor/ui/ViewerParameterDialog.java
+++ src/main/java/com/tascape/reactor/ui/ViewerParameterDialog.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.tascape.qa.th.ui;
+package com.tascape.reactor.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
